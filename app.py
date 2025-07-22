d893981f3225951c42bb69ad63ac7fd330aca915
sumitgujral <smtgjrl@gmail.com>
2025-07-23 00:31:56 +0530

    port updated , database updated, icon added 2

--- app.py
+++ app.py
@@ -592,5 +592,4 @@ def delete_medical_record(record_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
